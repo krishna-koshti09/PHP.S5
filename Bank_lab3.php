0fd9d932c97b1dbf114229f04046a22a2e0fe59a
Dhruv Sadrawala <115005984+Dhruv-Sadrawala@users.noreply.github.com>
2024-07-23 12:21:30 +0530

    Add files via upload

---
+++ Bank_lab3.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Bank Account</title>
+</head>
+<body>
+    <form method="POST">
+        <h2>Bank Account</h2>
+        Enter account number: <input type="text" name="accountNumber" required><br>
+        Enter amount: <input type="number" name="amt" required><br>
+        Choose Operation<br>
+        <select>
+            <option>Deposit</option>
+            <option>Withdrawl</option>
+        </select>
+        <input type="radio" name="transaction" value="deposit" required> Deposit <br>
+        <input type="radio" name="transaction" value="withdraw" required> Withdrawl <br>
+        <input type="submit" name="submit" value="submit">
+    </form>
+
+    <?php
+    if (isset($_POST['submit'])) {
+        
+        $ac_no=$_POST['accountNumber'];
+        $amt=$_POST['amt'];
+        $tra=$_POST['transaction'];
+        $bal=10000;
+
+        class Bank{
+
+            function deposit($bal,$amt){
+                $bal+=$amt;
+                echo "Balance after Deposit is:$bal";
+            }
+
+            function withdraw($bal,$amt){
+                $bal-=$amt;
+                echo "Balance after Withdrawl is:$bal";
+            }
+        }
+
+        $obj=new Bank();
+
+        if ($tra=='deposit') {
+            $obj->deposit($bal,$amt);
+        }
+        else
+            $obj->withdraw($bal,$amt);
+    }
+    ?>
+</body>
+</html>
